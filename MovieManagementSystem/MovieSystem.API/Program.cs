3740aafd9db9176d0c7dbdb0374c0d771eaca497
Aya Farag <ayasaid94.as@gmail.com>
2024-10-16 20:59:26 +0300

    Setup custom configration

--- MovieManagementSystem/MovieSystem.API/Program.cs
+++ MovieManagementSystem/MovieSystem.API/Program.cs
@@ -1,6 +1,17 @@
+using MovieSystem.Infrastructure.Presistance.Configrations;
+using MovieSystem.Application.Configrations;
+using MovieSystem.API.Configrations;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddAPIServices(builder.Configuration);
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +26,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
